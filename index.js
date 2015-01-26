23d3f406f7f1e0bf284d97cea55f4b6574554f8f
Christian Blaschke <mail@platdesign.de>
2015-01-26 06:36:23 +0100

    some changes

--- index.js
+++ index.js
@@ -1,78 +1,259 @@
 'use strict';
 
+require('colors');
+var extend = require('extend');
+var gutil = require('gulp-util');
 var async = require('async');
-var transform = require('vinyl-transform');
-var path = require('path');
-var fs = require('fs');
-var header = require('gulp-header');
+var watch = require('gulp-watch');
 
-module.exports = function pdGulpBaseTask(options) {
+var baseTask = function(pluginName, configFn) {
 
+	var Klasse = function() {
 
-	function Task(input) {
-		var that = this;
-
-		this.helper = helper;
-
-		this.jobsConfigs = [];
-		this.options = {};
-
-		Object.keys(input).forEach(function(key) {
-
-			if(key === 'options') {
-				that.options = input[key];
-			} else {
-				that.jobsConfigs.push(input[key]);
-			}
-
-		});
-	}
-	Task.create = function(options) {
-		return new Task(options);
-	};
-
-	Task.prototype.gulpHandler = function() {
-		var that = this;
-
-		var jobs = [];
-
-		this.jobsConfigs.forEach(function(config) {
-			jobs.push(function(callback) {
-				that.worker(config, callback);
+		this.watchStarter(function(job, compile) {
+			watch(job.config.watch===true ? job.config.src : job.config.watch, function() {
+				compile(job);
 			});
 		});
 
-		return function(callback) {
-			async.series(jobs, callback);
-		};
-	};
-	Task.prototype.worker = function(fn) {
-		this.worker = fn;
 	};
 
+	Klasse.prototype.init = function(fn) {
+		this._jobInitializator = fn;
+	};
 
-	return Task.create(options);
+	Klasse.prototype.worker = function(worker) {
+		this._worker = worker;
+	};
 
-};
+	Klasse.prototype.appendTask = function(taskName, options, registerHandler) {
+		this._register = this._register || [];
 
-
-
-
-
-var helper = {
-
-	useOnStream: function(fn, stream) {
-		return stream.pipe( transform(function(filename) {
-			return fn.apply(stream, [stream]);
-		}) );
-	},
-
-	banner: function(template) {
-
-		return header(template, {
-			pkg: JSON.parse( fs.readFileSync('package.json', 'utf8') ),
-			date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+		this._register.push({
+			name: taskName,
+			fn: registerHandler || function(handler, options) {
+				this.appendGulpTask(taskName, handler(options));
+			},
+			options: options
 		});
-	}
+	};
+
+	Klasse.prototype.watchStarter = function(fn) {
+		this._watchStarter = fn;
+	};
+
+	Klasse.prototype.appendGulpTask = function(taskName, deps, fn) {
+
+		if(!fn && deps) {
+			fn = deps;
+			deps = null;
+		}
+
+		var gulp = this.gulp;
+
+		var task = gulp.tasks[taskName] = gulp.tasks[taskName] || {
+			fn: function(cb) { cb(); },
+			dep:[],
+			name: taskName
+		};
+
+		if(deps) {
+			task.dep = task.dep.concat(deps);
+		}
+
+		if(fn) {
+			var oldFn = task.fn;
+			task.fn = function(cb) {
+				oldFn(function(err) {
+					if(err) { return cb(err); }
+
+					fn(cb);
+				});
+			};
+		}
+
+	};
+
+
+
+
+	Klasse.prototype.options2jobs = function(hash) {
+		var that = this;
+
+		var options = hash.options || {};
+		var jobs = [];
+
+		Object.keys(hash).forEach(function(key) {
+			if(key !== 'options') {
+
+				var job = {
+					name: pluginName+':'+key,
+					config: hash[key],
+					options: options
+				};
+
+
+				var initializeJob = function() {
+					job.initialized = true;
+					that.log('Initialized subtask '+job.name.cyan, '✔'.green);
+				};
+
+
+				jobs.push(function(asyncCallback) {
+					var noCallback = false;
+					var compileCallback = function(err) {
+
+						if(err) {
+							that.logError('Error in '.red+job.name.cyan, '✕'.red);
+							console.log('Message\n', err.message, '\n');
+						} else {
+							that.log('Compiled '+job.name.cyan, '✔'.green);
+						}
+
+						if(!noCallback) {
+							asyncCallback();
+						}
+
+						noCallback = true;
+					};
+
+					function compile() {
+						var args = [].slice.call(arguments).concat(compileCallback);
+
+						if(!job.initialized) {
+							initializeJob();
+						}
+
+						that.log('Compiling '+job.name.cyan, '...');
+						that._worker.apply(that, args);
+					}
+
+					function jobInitializator(job, compile) {
+						compile(job);
+
+						if(job.config.watch) {
+							that._watchStarter(job, compile);
+						}
+					}
+					if(that._jobInitializator) {
+						jobInitializator = that._jobInitializator;
+					}
+
+					jobInitializator(job, compile, initializeJob);
+
+				});
+
+
+			}
+		});
+
+		return jobs;
+
+	};
+
+
+	Klasse.prototype.runWorkerOnOptions = function(options, cb) {
+
+		async.series( this.options2jobs(options), cb);
+
+	};
+
+
+	var loggingPrefix = '    ⌊────➤ ';
+
+	Klasse.prototype.log = function() {
+		console.log.apply(console, [loggingPrefix.cyan].concat([].slice.call(arguments)));
+	};
+	Klasse.prototype.logError = function() {
+		console.log.apply(console, [loggingPrefix.red].concat([].slice.call(arguments)));
+	};
+
+
+
+	Klasse.prototype.plugin = function(name, opts) {
+		opts = opts || {};
+
+		if(baseTask.plugin[name]) {
+			return baseTask.plugin[name].apply(this, [opts]) || gutil.noop();
+		}
+
+	};
+
+
+	Klasse.prototype.publicApi = function() {
+		var that = this;
+		var api = function(options) {
+			return function(cb) {
+				try {
+					that.runWorkerOnOptions(options, cb);
+				}catch(e) {
+					cb(e);
+				}
+
+			};
+		};
+
+
+		/**
+		 * Register default tasks
+		 * @param  {[type]} options [description]
+		 *    {
+		 *    	options: {
+		 *    		banner: 'template'
+		 *    	},
+		 *    	default: {
+		 *
+		 *    	},
+		 *    	watch: {
+		 *
+		 *    	},
+		 *    	build: {
+		 *    	}
+		 *    }
+		 *
+		 *
+		 * @return {[type]}         [description]
+		 */
+		api.register = function(jobOptions, taskDefaultOptions) {
+
+			taskDefaultOptions = taskDefaultOptions || {};
+
+			that._register.forEach(function(item) {
+				var taskOptions = {};
+
+				Object.keys(jobOptions).forEach(function(key) {
+					if(key === 'options') {
+						taskOptions.options = jobOptions.options;
+					} else {
+						taskOptions[key] = extend(true, {}, jobOptions[key], item.options, taskDefaultOptions[item.name]);
+					}
+				});
+
+				item.fn.apply(that, [api, taskOptions]);
+
+			});
+
+		};
+
+		return api;
+	};
+
+	return function(gulp) {
+		var instance = new Klasse();
+
+		instance.gulp = gulp;
+
+		configFn.apply(instance, [instance]);
+
+		return instance.publicApi();
+	};
 
 };
+
+
+baseTask.plugin = {};
+
+baseTask.plugin.banner = require('./lib/plugins/banner.js');
+
+
+module.exports = baseTask;
